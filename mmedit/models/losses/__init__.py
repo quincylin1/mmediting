2fd1e2e47911da5c9db862e620872951730d57ca
lintsuihin <lintsuihin@sensetime.com>
2021-12-20 17:37:39 +0800

    ^X

--- mmedit/models/losses/__init__.py
+++ mmedit/models/losses/__init__.py
@@ -1,7 +1,7 @@
 from .composition_loss import (CharbonnierCompLoss, L1CompositionLoss,
                                MSECompositionLoss)
 from .gan_loss import (DiscShiftLoss, GANLoss, GaussianBlur,
-                       GradientPenaltyLoss, SMGANLoss)
+                       GradientPenaltyLoss)
 from .gradient_loss import GradientLoss
 from .perceptual_loss import PerceptualLoss, PerceptualVGG
 from .pixelwise_loss import CharbonnierLoss, L1Loss, MaskedTVLoss, MSELoss
@@ -9,7 +9,7 @@ from .utils import mask_reduce_loss, reduce_loss
 
 __all__ = [
     'L1Loss', 'MSELoss', 'CharbonnierLoss', 'L1CompositionLoss',
-    'MSECompositionLoss', 'CharbonnierCompLoss', 'GANLoss', 'SMGANLoss',
+    'MSECompositionLoss', 'CharbonnierCompLoss', 'GANLoss',
     'GaussianBlur', 'GradientPenaltyLoss', 'PerceptualLoss', 'PerceptualVGG',
     'reduce_loss', 'mask_reduce_loss', 'DiscShiftLoss', 'MaskedTVLoss',
     'GradientLoss'
